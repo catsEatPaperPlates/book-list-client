47046d7effa61cc120a419d07c9c538efe06fcd6
Jesse Atay <jesse.atay@gmail.com>
2018-04-16 19:45:02 -0700

    updates to client view.

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -6,3 +6,26 @@ ENV.isProduction = window.location.protocol === 'window.location.href';
 ENV.productionApiUrl = 'https://ja-booklist.herokuapp.com/';
 ENV.developmentApiUrl = 'http://localhost:5432/';
 ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
+
+(function(module) {
+  
+    Book.prototype.toHtml = function() {
+      let template = Handlebars.compile($('#book-template').text());
+      return template(this);
+    }
+  
+    Book.all = [];
+  
+    Book.loadAll = rows => {
+      Book.all = rows.sort((a, b) => b.title - a.title).map(book => new Book(book));
+    }
+  
+    Book.fetchAll = callback =>
+      $.get(`${ENV.apiUrl}/books`)
+        .then(Book.loadAll)
+        .then(callback)
+        .catch(errorCallback);
+  
+    module.Book = Book;
+  })(app)
+ 
